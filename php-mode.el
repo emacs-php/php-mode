emacs-php/php-mode

commit 3115f68417f10c0318bbc3e4df34c3d40b774cd1

Fontify the ‘use <namespace> as <alias>’ construct

php-mode.el
@@ -819,7 +819,11 @@ current `tags-file-name'."
     ;; FIX to handle implementing multiple
     ;; currently breaks on "class Foo implements Bar, Baz"
     '("\\<\\(new\\|extends\\|implements\\)\\s-+\\$?\\(\\sw+\\)"
-      (1 font-lock-keyword-face) (2 font-lock-type-face))
+      (1 font-lock-keyword-face) (2 font-lock-type-face nil t))
+
+    ;; namespace imports
+    '("^\\s-*use\\(\\(?:\\sw\\|\\\\\\)+\\)\\s-+as\\s-+(\\(?:\\sw\\|\\\\\\)+\\"
+      (1 font-lock-keyword-face) (2-font-lock-type-face))
 
     ;; function declaration
     '("\\<\\(function\\)\\s-+&?\\(\\sw+\\)\\s-*("

commit 64e4b031ec9feec6b2bfe34d48efb39ca9437d21

Fix most definitions in php-font-lock-keywords-2
Commit 3115f684 (Fontify the ‘use <namespace> as <alias>’ construct)
introduced a bug in the regular expression used to fontify namespace
imports.  This bug caused all future font-lock definitions to fail,
such as those for highlighting things such as public methods.

php-mode.el
@@ -834,7 +834,7 @@ current `tags-file-name'."
       (1 font-lock-keyword-face) (2 font-lock-type-face nil t))
 
     ;; namespace imports
-    '("^\\s-*use\\(\\(?:\\sw\\|\\\\\\)+\\)\\s-+as\\s-+(\\(?:\\sw\\|\\\\\\)+\\"
+    '("^\\s-*use\\(\\(?:\\sw\\|\\\\\)+\\)\\s-+as\\s-+\\(?:\\sw\\|\\\\\\)+"
       (1 font-lock-keyword-face) (2-font-lock-type-face))
 
     ;; function declaration