3cab3b0f9bf46774bec7e044a1a0b36c0fb1a4a9
Eric James Michael Ritz <lobbyjones@gmail.com>
2012-09-15 02:48:25 -0400

    Add a test case for Github issue 18

---
+++ tests/issue-18.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Github Issue:    https://github.com/ejmr/php-mode/issues/18
+ *
+ * If we run indent-region on the code below then it should appear
+ * like so:
+ *
+ * $a = 'github';
+ *
+ * if ($a === 'github') {
+ *     header('Location: http://github.com');
+ * }
+ *
+ * This tests for a bug where '//' inside of a string would cause an
+ * indentation error.
+ */
+
+$a = 'github';
+
+if ($a === 'github') {
+    header('Location: http://github.com');
+}
