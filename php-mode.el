60db0bc3459e81244fb8ee825195fe29ef0bf71d
Eric James Michael Ritz <lobbyjones@gmail.com>
2012-09-19 00:05:39 -0400

    Fix syntax highlighting of multi-line strings
    The syntax table entries for single and double quote characters cause
    improper syntax hightlighting multi-line strings.  However, this patch
    is not a complete solution.  Those syntax entries were originally
    introduced in order to fix the big where single quotes in HTML cause
    problems with highlighting.  Removing these entries fixes one bug in
    exchange for re-introducing another.  Therefore we can only regard
    this as a temporary Band-Aid.
    
    Github-issue: 21

--- php-mode.el
+++ php-mode.el
@@ -467,8 +467,6 @@ This is was done due to the problem reported here:
           nil))              ; SYNTAX-BEGIN
 
   (modify-syntax-entry ?_    "_" php-mode-syntax-table)
-  (modify-syntax-entry ?'    "w" php-mode-syntax-table)
-  (modify-syntax-entry ?\"   "w" php-mode-syntax-table)
   (modify-syntax-entry ?`    "\"" php-mode-syntax-table)
 
   (set (make-local-variable 'font-lock-syntactic-keywords)
