15261105c58ca4fca706b3b4761c2426d87e7d39
Eric James Michael Ritz <lobbyjones@gmail.com>
2012-10-15 00:06:33 -0400

    Update the version to 1.7

--- php-mode.el
+++ php-mode.el
@@ -10,7 +10,7 @@
 ;; Created: 1999-05-17
 ;; X-URL:   https://github.com/ejmr/php-mode
 
-(defconst php-mode-version-number "1.6.6"
+(defconst php-mode-version-number "1.7"
   "PHP Mode version number.")
 
 (defconst php-mode-modified "2012-10-15"
