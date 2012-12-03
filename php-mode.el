9de7244dccc69f38be0a23804fe4614fa3462488
Eric James Michael Ritz <lobbyjones@gmail.com>
2012-12-03 18:03:15 -0500

    Use run-mode-hooks instead of run-hooks
    Section ‘23.2.6 Mode Hooks’ of the Emacs Lisp Manual suggests this as
    proper behavior for derived major modes, which php-mode is.

--- php-mode.el
+++ php-mode.el
@@ -541,7 +541,7 @@ This is was done due to the problem reported here:
   (set (make-local-variable 'add-log-current-defun-header-regexp)
        php-beginning-of-defun-regexp)
 
-  (run-hooks 'php-mode-hook))
+  (run-mode-hooks 'php-mode-hook))
 
 ;; Make a menu keymap (with a prompt string)
 ;; and make it the menu bar item's definition.
