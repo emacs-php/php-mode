ffa5139a30495a6ac0f7c9ba43b5e9a8d8bf416e
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-02-15 17:21:00 -0500

    Update php-mode-modified for ‘function’ keyword addition

--- php-mode.el
+++ php-mode.el
@@ -11,7 +11,7 @@
 (defconst php-mode-version-number "1.10"
   "PHP Mode version number.")
 
-(defconst php-mode-modified "2013-02-06"
+(defconst php-mode-modified "2013-02-15"
   "PHP Mode build date.")
 
 ;;; License
