db4ea65f24cad0cabbe4fffe5a55276e9a6b22a3
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-03-15 20:16:11 -0400

    Do not trigger search error in php-mode-test-issue-19
    We search for the string ‘$object->’ to determine if its position
    matches the arrow on the next line.  Because we test this in different
    contexts (e.g. in methods, in closures, at the top level) we search
    for the string in a loop.  When we reach the end of the buffer,
    however, search-forward returns an error because there are no more
    ‘$object->’ strings to find.  This patch changes the search so it does
    not raise the error but only returns nil at the end, causing the loop
    to end normally.
    
    Github-issue: 81

--- php-mode-test.el
+++ php-mode-test.el
@@ -125,11 +125,11 @@ Gets the face of the text after the comma."
 (ert-deftest php-mode-test-issue-19 ()
   "Alignment of arrow operators."
   (with-php-mode-test ("issue-19.php" :indent t)
-    (while (re-search-forward "^\\s-*\\$object->")
+    (while (search-forward "$object->" (point-max) t)
       ;; Point is just after `->'
       (let ((col (current-column)))
         (search-forward "->")
-        (should (eq (current-column) col))))))
+        (should (= (current-column) col))))))
 
 (ert-deftest php-mode-test-issue-21 ()
   "Font locking multi-line string."
