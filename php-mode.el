88cfffcd6f76838d2048b2951463ad3c6ea7dc25
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-08-16 14:17:07 -0400

    Update php-mode-modified for changes made today

--- php-mode.el
+++ php-mode.el
@@ -11,7 +11,7 @@
 (defconst php-mode-version-number "1.11"
   "PHP Mode version number.")
 
-(defconst php-mode-modified "2013-07-18"
+(defconst php-mode-modified "2013-08-16"
   "PHP Mode build date.")
 
 ;;; License
