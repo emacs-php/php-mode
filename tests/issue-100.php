4b7f130a0b30c289ebe6e3e2018750767f53c8aa
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-08-26 11:47:05 -0400

    GitHub-Issue: 100 (Unit Test)

---
+++ tests/issue-100.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * GitHub Issue:    https://github.com/ejmr/php-mode/issues/100
+ *
+ * The code below should appear like so:
+ *
+ *     use ReflectionMethod,
+ *         ReflectionObject,
+ *         ReflectionProperty;
+ *
+ * Each namespace must have the correct font-face.  Aligning the
+ * namespaces is not required to satisfy the test but is desirable.
+ * 
+ */
+
+use ReflectionMethod,
+    ReflectionObject,
+    ReflectionProperty;
