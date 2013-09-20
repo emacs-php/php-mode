c1c35d3efd125f23f87ac7923beb97fbaef9c992
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-09-20 18:42:54 -0400

    Explain the expected behavior of the test for GitHub issue 124

--- tests/issue-124.php
+++ tests/issue-124.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * GitHub Issue:    https://github.com/ejmr/php-mode/issues/124
+ *
+ * PHP Mode must highlight the heredoc string below using
+ * font-lock-string-face.  The presence of the apostrophe or anything
+ * else within the string should not stop the highlighting from
+ * leaking out beyond the ending 'EOT' marker.
+ * 
+ */
+
 $foo = <<<EOT
 Heredoc with a ' inside.
 EOT;
