b1dc741e4d3a4fe45531ea680b8d477143235b0b
Eric James Michael Ritz <ejmr@plutono.com>
2014-05-23 22:47:52 -0400

    Bump the version number thanks to contributions from Bence Kalmar

--- php-mode.el
+++ php-mode.el
@@ -6,9 +6,9 @@
 
 ;;; Author: Eric James Michael Ritz
 ;;; URL: https://github.com/ejmr/php-mode
-;;; Version: 1.13.1
+;;; Version: 1.13.2
 
-(defconst php-mode-version-number "1.13.1"
+(defconst php-mode-version-number "1.13.2"
   "PHP Mode version number.")
 
 (defconst php-mode-modified "2014-05-19"
