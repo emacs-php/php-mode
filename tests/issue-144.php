34db88b0e7b6f41e6cdaf4c377c71e018721835d
Syohei YOSHIDA <syohex@gmail.com>
2014-08-26 10:55:32 +0900

    Add regression test of #144

---
+++ tests/issue-144.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * GitHub Issue:    https://github.com/ejmr/php-mode/issues/144
+ *
+ * Indentation test '#' comment line has single quote.
+ *
+ */
+
+# This project's homepage is  http://www.website.com
+$a = 1;
+if( $a == 1 ) {
+$a = $a + 1;
+}
+?>
