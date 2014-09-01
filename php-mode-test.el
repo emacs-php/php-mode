e86c35b07e3130879ed65f3d443ea50fe8c79990
Joris Steyn <jorissteyn@gmail.com>
2014-09-01 20:11:24 +0200

    Exclude new ert tests depending on fl-add-keywords

--- php-mode-test.el
+++ php-mode-test.el
@@ -296,60 +296,62 @@ style from Drupal."
   "Closure indentation."
   (with-php-mode-test ("issue-145.php" :indent t)))
 
-(ert-deftest php-mode-test-constants ()
-  "Proper highlighting for constants."
-  (custom-set-variables '(php-extra-constants (quote ("extraconstant"))))
-  (with-php-mode-test ("constants.php")
-    (let ((variables '(
-                       "true" "TRUE"
-                       "false" "FALSE"
-                       "null" "NULL"
-                       "IS_CONSTANT"
-                       "__IS_CONSTANT__"
-                       "IS_CONSTANT99"
-                       "extraconstant")))
-      (dolist (variable variables)
-        (search-forward variable)
-        (goto-char (match-beginning 0))
-        (should (eq 'font-lock-constant-face
-                    (get-text-property (point) 'face))))))
-  (custom-set-variables '(php-extra-constants (quote ())))
-  (with-php-mode-test ("constants.php")
-    (let ((variables '("no_constant"
-                       "no_CONSTANT"
-                       "extraconstant")))
-      (dolist (variable variables)
-        (search-forward variable)
-        (goto-char (match-beginning 0))
-        (should (not (eq 'font-lock-constant-face
-                    (get-text-property (point) 'face))))))))
+(when (php-mode-test-support-font-lock-add-keywords-p)
+  (ert-deftest php-mode-test-constants ()
+    "Proper highlighting for constants."
+    (custom-set-variables '(php-extra-constants (quote ("extraconstant"))))
+    (with-php-mode-test ("constants.php")
+      (let ((variables '(
+                         "true" "TRUE"
+                         "false" "FALSE"
+                         "null" "NULL"
+                         "IS_CONSTANT"
+                         "__IS_CONSTANT__"
+                         "IS_CONSTANT99"
+                         "extraconstant")))
+        (dolist (variable variables)
+          (search-forward variable)
+          (goto-char (match-beginning 0))
+          (should (eq 'font-lock-constant-face
+                      (get-text-property (point) 'face))))))
+    (custom-set-variables '(php-extra-constants (quote ())))
+    (with-php-mode-test ("constants.php")
+      (let ((variables '("no_constant"
+                         "no_CONSTANT"
+                         "extraconstant")))
+        (dolist (variable variables)
+          (search-forward variable)
+          (goto-char (match-beginning 0))
+          (should (not (eq 'font-lock-constant-face
+                       (get-text-property (point) 'face)))))))))
 
-(ert-deftest php-mode-test-identifiers()
-  "Proper highlighting for identifiers including their namespace."
-  (with-php-mode-test ("identifiers.php")
-    (let ((variables '("UnqualifiedClassName"
-                       "FullyQualifiedClassName"
-                       "SpaceName")))
-      (dolist (variable variables)
-        (search-forward variable)
-        (goto-char (match-beginning 0))
-        (should (eq 'font-lock-type-face
-                    (get-text-property (point) 'face)))))
-    (search-forward "var")
-    (goto-char (match-beginning 0))
-    (should (eq 'font-lock-variable-name-face
-                (get-text-property (point) 'face)))
-    (search-forward "syntaxerror")
-    (goto-char (match-beginning 0))
-    (should (not (eq 'font-lock-variable-name-face
-                     (get-text-property (point) 'face))))
-    (search-forward "ClassName")
-    (goto-char (match-beginning 0))
-    (should (eq 'font-lock-constant-face
-                (get-text-property (point) 'face)))
-    (search-forward "SpaceName")
-    (goto-char (match-beginning 0))
-    (should (eq 'font-lock-constant-face
-                (get-text-property (point) 'face)))))
+(when (php-mode-test-support-font-lock-add-keywords-p)
+  (ert-deftest php-mode-test-identifiers()
+    "Proper highlighting for identifiers including their namespace."
+    (with-php-mode-test ("identifiers.php")
+      (let ((variables '("UnqualifiedClassName"
+                         "FullyQualifiedClassName"
+                         "SpaceName")))
+        (dolist (variable variables)
+          (search-forward variable)
+          (goto-char (match-beginning 0))
+          (should (eq 'font-lock-type-face
+                      (get-text-property (point) 'face)))))
+      (search-forward "var")
+      (goto-char (match-beginning 0))
+      (should (eq 'font-lock-variable-name-face
+                  (get-text-property (point) 'face)))
+      (search-forward "syntaxerror")
+      (goto-char (match-beginning 0))
+      (should (not (eq 'font-lock-variable-name-face
+                       (get-text-property (point) 'face))))
+      (search-forward "ClassName")
+      (goto-char (match-beginning 0))
+      (should (eq 'font-lock-constant-face
+                  (get-text-property (point) 'face)))
+      (search-forward "SpaceName")
+      (goto-char (match-beginning 0))
+      (should (eq 'font-lock-constant-face
+                  (get-text-property (point) 'face))))))
 
 ;;; php-mode-test.el ends here
--- php-mode.el
+++ php-mode.el
@@ -62,6 +62,7 @@
 ;;; Code:
 
 (require 'cc-mode)
+(require 'cl)
 (eval-when-compile
   (require 'cc-langs)
   (require 'cc-fonts))
