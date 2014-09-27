3440196a860d02bdc34bdb90929b7f21f11842a9
Syohei YOSHIDA <syohex@gmail.com>
2014-09-27 10:47:01 +0900

    Don't highlight number as constant(#175)
    Signed-off-by: Eric James Michael Ritz <ejmr@plutono.com>

---
+++ tests/issue-175.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * GitHub Issue:    https://github.com/ejmr/php-mode/issues/175
+ *
+ * PHP mode should not highlight more than 2 digit charcter
+ *
+ */
+
+return [1,2,3,4,5,6,7,8,9,10];
