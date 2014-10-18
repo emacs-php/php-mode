fd424d9a90e381888d74142bc84b6399d54f0562
Joris Steyn <jorissteyn@gmail.com>
2014-10-18 21:27:01 +0200

    Add missing language constructs to php-mode
    A new test contains all language constructs and reserved keywords from
    the PHP 5.6 manual. The test checks that they all get the
    keyword-face. In addition to that, for keywords that preceed a type
    identifier like instanceof and insteadof, it is checked that the
    identifier gets the type-face.
    
    Some keywords in c-other-kwds were already specified in another *-kwds,
    those are now removed. Keywords added in this commit are: insteadof,
    callable (the type hint now works just like 'array') and __halt_compiler().
    
    This resolves issue #180.

---
+++ tests/language-constructs.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Test highlighting of language constructs and reserved keywords
+ *
+ * This test is based on http://php.net/manual/en/reserved.keywords.php and should
+ * be updated from the manual when new PHP versions arrive. Built-in functions are
+ * not treated differently by php-mode than regular function calls: that means
+ * they are not highlighted. Only language constructs like print/die and reserved
+ * keywords are, and those are tested here.
+ */
+
+// Start:
+__halt_compiler();
+abstract;
+and;
+array();
+as;
+break;
+callable;
+case;
+catch;
+class ClassName;
+clone;
+const;
+continue;
+declare;
+default;
+die();
+do;
+echo;
+else;
+elseif;
+empty();
+enddeclare;
+endfor;
+endforeach;
+endif;
+endswitch;
+endwhile;
+eval();
+exit();
+extends ClassName;
+final;
+finally;
+for;
+foreach;
+function;
+global;
+goto;
+if;
+implements ClassName;
+include;
+include_once;
+instanceof ClassName;
+insteadof ClassName;
+interface ClassName;
+isset();
+list();
+namespace ClassName;
+new ClassName;
+print;
+private;
+protected;
+public;
+require;
+require_once;
+return;
+static;
+switch;
+throw;
+trait ClassName;
+try;
+unset();
+use ClassName;
+var;
+while;
+xor;
+yield;
