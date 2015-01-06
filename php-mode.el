b96d244a3ed99e1e4b28023b9c86bbc8bc0f697c
l3msh0 <l3msh0@gmail.com>
2015-01-06 21:59:01 +0900

    Fix regexp for imenu to show static method properly
    Add test case for issue #83.
    
    Though we can place 'static' keyword either before or after a visibility
    keyword at method definition, imenu has only shown the latter case. This
    commit fixes 'php-imenu-generic-expression' for the methods of both case
    to appear on imenu.
    
    GitHub-Issue: 83
    
    Signed-off-by: Eric James Michael Ritz <ejmr@plutono.com>

--- php-mode.el
+++ php-mode.el
@@ -11,7 +11,7 @@
 (defconst php-mode-version-number "1.15.1"
   "PHP Mode version number.")
 
-(defconst php-mode-modified "2014-12-29"
+(defconst php-mode-modified "2015-01-06"
   "PHP Mode build date.")
 
 ;;; License
@@ -191,10 +191,8 @@ which will be the name of the method."
   (concat
    ;; Initial space with possible 'abstract' or 'final' keywords
    "^\\s-*\\(?:\\(?:abstract\\|final\\)\\s-+\\)?"
-   ;; The function visilibity
-   visibility
-   ;; Is it static?
-   "\\s-+\\(?:static\\s-+\\)?"
+   ;; 'static' keyword may come either before or after visibility
+   "\\(?:" visibility "\\(?:\\s-+static\\)?\\|\\(?:static\\s-+\\)?" visibility "\\)\\s-+"
    ;; Make sure 'function' comes next with some space after
    "function\\s-+"
    ;; Capture the name as the first group and the regexp and make sure
