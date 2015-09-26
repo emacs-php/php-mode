c9d32dd1d1ade4ba27996ace46c9be85d0c066ba
Syohei YOSHIDA <syohex@gmail.com>
2015-09-27 00:16:35 +0900

    Update modified date

--- php-mode.el
+++ php-mode.el
@@ -11,7 +11,7 @@
 (defconst php-mode-version-number "1.17.0"
   "PHP Mode version number.")
 
-(defconst php-mode-modified "2015-09-17"
+(defconst php-mode-modified "2015-09-27"
   "PHP Mode build date.")
 
 ;;; License
