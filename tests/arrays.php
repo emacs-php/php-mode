56a7e7c67b70c4aba5919317a8898157ba0b08e4
fabacino <fabacino@gmail.com>
2017-04-07 14:50:17 +0200

    Improve highlighting of type hints and return types
    * Highlight `void` and `array` as types when used as type hints or
      return types (previously highlighted as keywords).
    
    * Highlight nullable class names as types when used as type hints or
      return types (previously not highlighted).
    
    Signed-off-by: Eric James Michael Ritz <ericjmritz@yandex.com>

--- tests/arrays.php
+++ tests/arrays.php
@@ -3,8 +3,14 @@
 // Array should be treated like a normal keyword
 $test = array();
 
-$test = function(array $test = array()) {
+$test = function($test = array()) {
 };
 
-// Unless it's a cast, then it should behave like (int)
+// Array should be treated as a type instead of a keyword in the following situations:
+// - cast, should look like `(int)`
+// - type hint, should look like `int $var`
+// - return type, should look like `: int`
 $test = (array)$test;
+
+$test = function(array $test): array {
+};
