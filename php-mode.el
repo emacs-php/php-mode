cce786c881b302de89cf5c2dfee73f9b54c400e9
USAMI Kenta <tadsan@pixiv.com>
2018-02-10 04:24:21 +0900

    Use "Emacs Lisp" instead "lisp", that pointed out by checkdoc

--- php-mode.el
+++ php-mode.el
@@ -44,7 +44,7 @@
 
 ;; ## Usage
 
-;; Put this file in your Emacs lisp path (eg. site-lisp) and add to
+;; Put this file in your Emacs Lisp path (eg. site-lisp) and add to
 ;; your .emacs file:
 
 ;;   (require 'php-mode)
