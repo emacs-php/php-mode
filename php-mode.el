0ab1a2cf7c20c96c4f55b734925e595db9ce6aca
USAMI Kenta <tadsan@pixiv.com>
2018-02-12 01:52:26 +0900

    Fix c-before-font-lock-functions
    Before change, return of functions is forgotten.
    In my opinion, the original correct code is as follows.
    
    ```
    (let (functions)
      (when (fboundp #'c-change-expand-fl-region)
        (cl-pushnew 'c-change-expand-fl-region functions))
      (when (fboundp #'c-depropertize-new-text)
        (cl-pushnew 'c-depropertize-new-text functions))
      functions)
    ```
    
    Using cl-loop instead of when and cl-pushnew makes the code clearer.

--- php-mode.el
+++ php-mode.el
@@ -485,11 +485,9 @@ SYMBOL
   php nil)
 
 (c-lang-defconst c-before-font-lock-functions
-  php (let (functions)
-        (when (fboundp #'c-change-expand-fl-region)
-          (cl-pushnew 'c-change-expand-fl-region functions))
-        (when (fboundp #'c-depropertize-new-text)
-          (cl-pushnew 'c-depropertize-new-text functions))))
+  php (cl-loop for f in '(c-change-expand-fl-region c-depropertize-new-text)
+               if (fboundp f)
+               collect f))
 
 ;; Make php-mode recognize opening tags as preprocessor macro's.
 ;;
