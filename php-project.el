9ed89c862835b406c460747e11ca0e8b3826077a
USAMI Kenta <tadsan@pixiv.com>
2018-02-28 11:23:43 +0900

    Add emptiy php-project.el
    This package is part of php-mode but will be provided as a separate
    package to interoperate with another major mode in the future.

---
+++ php-project.el
@@ -0,0 +1,43 @@
+;;; php-project.el --- Project support for PHP application  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Friends of Emacs-PHP development
+
+;; Author: USAMI Kenta <tadsan@zonu.me>
+;; Keywords: tools, files
+;; URL: https://github.com/ejmr/php-mode
+;; Version: 1.18.4
+;; Package-Requires: ((emacs "24") (cl-lib "0.5"))
+;; License: GPL-3.0-or-later
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Define project specific functions and variables for PHP application.
+
+;;; Code:
+
+
+;; Variables
+
+
+
+;; Buffer local variables
+
+
+;; Functions
+
+
+(provide 'php-project)
+;;; php-project.el ends here
