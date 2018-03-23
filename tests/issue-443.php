63685bf69f1fe48914be9f48fe484cfe13110eb5
USAMI Kenta <tadsan@pixiv.com>
2018-03-23 15:23:19 +0900

    Add test file related to #443

---
+++ tests/issue-443.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1); ?>
+<?xml version="1.0" encoding="UTF-8"?>
+<?hh echo "Hack open tag" ?>
+<? echo "obsolete short open tag" ?>
+<% echo "obsolete ASP tags" %>
+<?= "foo" ?>
+<?= 1 + 1 ?>
+<?php /* Foo */ echo "Bar"; ?>
+<?php/* Foo */ echo "Bar"; ?>
