4347837e9965c0eb7b3fb03fe7595a668498d57a
USAMI Kenta <tadsan@zonu.me>
2019-05-16 21:04:49 +0900

    Reclassify keywords and types

---
+++ tests/lang/types/cast.php
@@ -0,0 +1,15 @@
+<?php
+
+$var = 1;
+
+(array)$var;
+(binary)$var;
+(bool)$var;
+(boolean)$var;
+(float)$var;
+(int)$var;
+(integer)$var;
+(object)$var;
+(real)$var;
+(string)$var;
+(unset)$var;
---
+++ tests/lang/types/keywords.php
@@ -0,0 +1,14 @@
+<?php
+
+__halt_compiler();
+
+bool;
+boolean;
+int;
+integer;
+float;
+double;
+real;
+string;
+object;
+resource;
