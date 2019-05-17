83eb4a854f2667ff8a0cc5c8e78a27baea19daee
USAMI Kenta <tadsan@zonu.me>
2019-05-18 08:19:14 +0900

    Add php-constant-assign face to "const" statement
    It inherits font-lock-type-face for display compatibility. At the same
    time, restrictions on character types and number of characters were
    removed.

---
+++ tests/lang/constants/const.php
@@ -0,0 +1,19 @@
+<?php
+
+const A = 'A';
+const a = 'a';
+const B='B';
+const b='b';
+const Aa = 'Aa';
+const AA = 'AA';
+const AB='AB';
+const Ab='AA';
+const α = '';
+const Ａ = '';
+const ａ = '';
+const あ = 'あ';
+const 漢 = '漢';
+const あα = 'あα';
+const 漢_ = '漢_';
+const あ_='あ_';
+const X='X';const y='y';
