623ad520b99b3f87cea12ae10dc439383813a3aa
USAMI Kenta <tadsan@zonu.me>
2020-08-12 21:04:44 +0900

    Add php-mode-pkg.el for v1 branch

---
+++ php-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "php-mode" "1.23.0" "Major mode for editing PHP code"
+  '((emacs "24.3"))
+  :keywords
+  '("languages" "php")
+  :authors
+  '(("Eric James Michael Ritz"))
+  :maintainer
+  '("USAMI Kenta" . "tadsan@zonu.me")
+  :url "https://github.com/emacs-php/php-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
