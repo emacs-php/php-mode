943111a878095172adb60badbbd88d485881202d
takeokunn <bararararatty@gmail.com>
2022-11-03 19:09:04 +0900

    Fix

--- lisp/php-mode.el
+++ lisp/php-mode.el
@@ -1287,11 +1287,11 @@ for \\[find-tag] (which see)."
         "never" "never-return" "never-returns" "no-return" "non-empty-array"
         "non-empty-list" "non-empty-string" "non-falsy-string"
         "numeric" "numeric-string" "positive-int" "scalar"
-        "trait-string" "truthy-string" "self-out" "this-out"))
+        "trait-string" "truthy-string"))
 
 (defconst php-phpdoc-type-tags
   (list "package" "param" "property" "property-read" "property-write"
-        "return" "throws" "var"))
+        "return" "throws" "var" "self-out" "this-out"))
 
 (defconst php-phpdoc-font-lock-doc-comments
   `(("{@[-[:alpha:]]+\\s-*\\([^}]*\\)}" ; "{@foo ...}" markup.
