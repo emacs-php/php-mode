31220ab874be6faa796a2072c64a903e4f86854d
USAMI Kenta <tadsan@zonu.me>
2023-03-06 06:14:28 +0900

    Fix #726 indentation

---
+++ tests/indent/issue-726.php
@@ -0,0 +1,13 @@
+<?php
+$a = [
+    'aaa' => [
+        'bee' => 2,
+    ],
+    // 'foo' => [
+    //      'bar' => 1,
+    // ],
+    // ###php-mode-test### ((indent 4))
+    'lee' => 2,
+    // 'dee' => 3
+    'gee' => 4, // ###php-mode-test### ((indent 4))
+]; // ###php-mode-test### ((indent 0))
